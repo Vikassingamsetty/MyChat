a68b657166afc758a4b05a222ed547fa3358d145
Vikas Singamsetty <vikas@appcare.co.in>
2020-10-07 14:32:51 +0530

    Initial commit

---
+++ MyChat/Controllers/ConversationVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyChat
+//
+//  Created by vikas on 07/10/20.
+//  Copyright © 2020 vikas. All rights reserved.
+//
+
+import UIKit
+
+class ConversationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
