d8dc2fdc30ff6ea7ae342c3c9d8182ddaec3b083
Vikas Singamsetty <vikas@appcare.co.in>
2020-10-10 22:31:44 +0530

    Database email format is been added using stringoccurrence.

--- MyChat/Resources/DatabaseManager.swift
+++ MyChat/Resources/DatabaseManager.swift
@@ -14,18 +14,20 @@ final class DatabaseManager {
     static let shared = DatabaseManager()
     
     private let database = Database.database().reference()
-    
-    
-    
+  
 }
 
 //MARK:- Account Management
 extension DatabaseManager {
     
     public func userExists(with email:String, completion: @escaping ((Bool) -> Void)){
-        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             guard snapshot.value as? String != nil else {
-                completion(false)
+                completion(false) // already exist user
                 return
             }
             completion(true)
@@ -34,7 +36,7 @@ extension DatabaseManager {
     
     ///inserts new user to database
     public func inserUser(with user: ChatAppUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name":user.firstName,
             "last_name":user.lastName
         ])
@@ -45,5 +47,12 @@ struct ChatAppUser {
     let firstName:String
     let lastName:String
     let emailAddress:String
+    
+    var safeEmail:String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
+    
 //    let profilePictureUrl:String
 }
